10d32a9d0cddf71c04b211af2dd0dccb92e7dff7
Jacob Archambault <jacob.archambault@waystar.com>
2020-03-26 23:03:32 -0400

    Add OpenConnection and CloseConnection methods to InventoryDAL

--- AutoLotDAL/DataOperations/InventoryDAL.cs
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -11,8 +11,22 @@ namespace AutoLotDAL.DataOperations
     public class InventoryDAL
     {
         private readonly string _connectionString;
+        private SqlConnection _sqlConnection = null;
         public InventoryDAL() : this(@"Data Source = (localdb)\mssqllocaldb;IntegratedSecurity=true;Initial Catalog=AutoLot")
         { }
         public InventoryDAL(string connectionString) => _connectionString = connectionString;
+
+        private void OpenConnection() 
+        {
+            _sqlConnection = new SqlConnection { ConnectionString = _connectionString };
+            _sqlConnection.Open();
+        }
+        private void CloseConnection()
+        {
+            if (_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+        }
     }
 }
