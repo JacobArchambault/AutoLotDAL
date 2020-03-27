448a1114e9ad312a5a1eb1eeec2519584610ad30
Jacob Archambault <jacob.archambault@waystar.com>
2020-03-26 23:28:42 -0400

    Add GetAllInventory and GetCar methods to InventoryDAL

--- AutoLotDAL/DataOperations/InventoryDAL.cs
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using AutoLotDAL.Models;
 
 namespace AutoLotDAL.DataOperations
 {
@@ -27,6 +28,55 @@ namespace AutoLotDAL.DataOperations
             {
                 _sqlConnection?.Close();
             }
+        }
+        public List<Car> GetAllInventory()
+        {
+            OpenConnection();
+            // This will hold the records.
+            List<Car> inventory = new List<Car>();
+
+            // Prep command object.
+            string sql = "Select * From Inventory";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    inventory.Add(new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    });
+                }
+                dataReader.Close();
+            }
+            return inventory;
+        }
+        public Car GetCar(int id)
+        {
+            OpenConnection();
+            Car car = null;
+            string sql = $"Select * From Inventory where CarId = {id}";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while(dataReader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    };
+                }
+                dataReader.Close();
+            }
+            return car;
         }
     }
 }
