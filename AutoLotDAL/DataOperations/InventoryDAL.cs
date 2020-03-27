9d9b70beb2b3015448ed768b082c87c4397aa887
Jacob Archambault <jacob.archambault@waystar.com>
2020-03-27 19:51:14 -0400

    Add AutoLotClient console application to solution

--- AutoLotDAL/DataOperations/InventoryDAL.cs
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -163,5 +163,44 @@ namespace AutoLotDAL.DataOperations
             }
             CloseConnection();
         }
+        public string LookUpPetName(int carId)
+        {
+            OpenConnection();
+            string carPetName;
+
+            // Establish name of stored proc.
+            using (SqlCommand command = new SqlCommand("GetPetName", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                // Input param.
+                SqlParameter param = new SqlParameter
+                {
+                    ParameterName = "@carId",
+                    SqlDbType = SqlDbType.Int,
+                    Value = carId,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(param);
+
+                // Output param.
+                param = new SqlParameter
+                {
+                    ParameterName = "@petName",
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10,
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(param);
+
+                // Execute the stored procedure.
+                command.ExecuteNonQuery();
+
+                // Return output param.
+                carPetName = (string)command.Parameters["@petName"].Value;
+                CloseConnection();
+            }
+            return carPetName;
+        }
     }
 }
